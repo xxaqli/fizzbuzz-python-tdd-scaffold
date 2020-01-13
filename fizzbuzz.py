cbe1372eee330943345439c0dc0efaee7c194ea3
xxaqli <li_xxaq@163.com>
2020-01-13 21:09:07 +0800

    add_fizzbuzz_7

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,7 +4,7 @@ def process(number):
         return 'FizzBuzz'
     elif number % 3 == 0:
         return 'Fizz'
-    elif number % 5 ==0:
+    elif number % 5 == 0:
         return 'Buzz'
     else:
         return number
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,13 +5,10 @@ import unittest
 class TestFizzBuzz(unittest.TestCase):
     def test_multiple_of_three(self):
         self.assertEqual(fizzbuzz.process(6),'Fizz')
-    
     def test_multiple_of_five(self):
         self.assertEqual(fizzbuzz.process(20), 'Buzz')
-
     def test_multiple_of_five(self):
         self.assertEqual(fizzbuzz.process(15), 'FizzBuzz')
-
     def test_regular_numbers(self):
         self.assertEqual(fizzbuzz.process(2), 2)
         self.assertEqual(fizzbuzz.process(11), 11)
