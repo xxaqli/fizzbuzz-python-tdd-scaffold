8a8d2f51a13869eefa67c27fa63c0ee4822408b0
xxaqli <li_xxaq@163.com>
2020-01-13 23:17:33 +0800

    add_fizzbuzz10

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,15 +4,15 @@ import unittest
 
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_multiple_of_three(self):
+    def test_three(self):
         self.assertEqual(fizzbuzz.process(6), 'Fizz')
 
-    def test_multiple_of_five(self):
-        self.assertEqual(fizzbuzz.process(20), 'Buzz')
+    def test_five(self):
+        self.assertEqual(fizzbuzz.process(10), 'Buzz')
 
-    def test_multiple_of_five(self):
+    def test_three_and_five(self):
         self.assertEqual(fizzbuzz.process(15), 'FizzBuzz')
 
-    def test_regular_numbers(self):
+    def test_regular(self):
         self.assertEqual(fizzbuzz.process(2), 2)
         self.assertEqual(fizzbuzz.process(11), 11)
